cf63f748cd03214ec1624bbed127d3ead4779159
Leonardo <bandroid289@gmail.com>
2025-11-13 22:43:25 -0300

    t

---
+++ exercicio4cap2.py
@@ -0,0 +1,18 @@
+Largura = 17
+Altura = 12.0
+
+# 1
+resultado1 = Largura / 2
+print("Largura / 2 =", resultado1, "| tipo:", type(resultado1))
+
+# 2
+resultado2 = Largura // 2
+print("Largura // 2 =", resultado2, "| tipo:", type(resultado2))
+
+# 3
+resultado3 = Altura / 3
+print("Altura / 3 =", resultado3, "| tipo:", type(resultado3))
+
+# 4
+resultado4 = 1 + 2 * 5
+print("1 + 2 * 5 =", resultado4, "| tipo:", type(resultado4))
